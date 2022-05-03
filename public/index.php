fcb2256bd54e589b2efff2e57f802ace6ccfaac5
vvoleman <vojtech.voleman@tul.cz>
2022-05-03 21:03:18 +0200

    Fix CORS

--- public/index.php
+++ public/index.php
@@ -2,5 +2,5 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://vvoleman-stin-bot.herokuapp.com');
 \App\Bootstrap::run();
